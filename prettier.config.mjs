779ca50d4983cfb286826dd3a619e9c6fc91a326
okonomi <okonomi@oknm.jp>
2025-04-13 22:57:29 +0900

    feat: remove printWidth option

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   semi: false,
-  printWidth: 120,
   trailingComma: "es5",
   plugins: ["prettier-plugin-tailwindcss"],
   tailwindFunctions: ["clsx"],
-};
+}
 
-export default config;
+export default config
