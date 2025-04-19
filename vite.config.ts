92c2c666807b609a5193c90062930644198e067f
okonomi <okonomi@oknm.jp>
2025-04-19 14:26:10 +0900

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vite-react-ts-tailwind-starter/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
